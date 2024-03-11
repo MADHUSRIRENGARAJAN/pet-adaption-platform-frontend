7343b0d08d024fe2c91ed1bd895aa860bf228f9d
MADHUSRIRENGARAJAN <madhusri2317@gmail.com>
2024-03-11 04:27:49 -0700

    first commit

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://lh3.googleusercontent.com/proxy/i6LCAXx0N3bqSJIJpiqLa9oo-C24ATmcHDCp8axmQ6usGGCRFS-gYQDiNPR8EaJksaUcNFu6jBUO1l90UAroHqH3-WrpyS2q0R_pIqdVAlNyjnj9ayVKDNdbOkYZXTyoy_r2qSA" alt="Welcome picture" />
+    </div>
+  )
+}
